2855f444588dccfe059b5797953692f3e1edf5a7
felipebsales71-cmd <felipebsales71@gmail.com>
2026-08-23 16:43:53 -0400

    feat: add commercial configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+window.SALES_TECH_CONFIG = {
+  whatsappNumber: "5595XXXXXXXXX",
+  whatsappMessage: "Olá! Conheci a Sales Tech pelo site e gostaria de conversar sobre uma solução para minha empresa.",
+  instagramUrl: "https://instagram.com/SEUINSTAGRAM",
+  demoVideos: [
+    "",
+    "",
+    ""
+  ]
+};
