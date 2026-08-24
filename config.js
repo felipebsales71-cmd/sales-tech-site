0bbe41251c16364d2b93eb400654359b07a2b1e3
felipebsales71-cmd <felipebsales71@gmail.com>
2026-08-24 10:27:33 -0400

    Configure Sales Tech Instagram profile

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.SALES_TECH_CONFIG = {
   contactEmail: "sales.tech.rr.dev@gmail.com",
   whatsappNumber: "",
   whatsappMessage: "Olá! Conheci a Sales Tech pelo site e gostaria de conversar sobre uma solução para minha empresa.",
-  instagramUrl: "https://instagram.com/SEUINSTAGRAM",
+  instagramUrl: "https://instagram.com/salestech.rr",
   demoVideos: [
     "",
     "",
