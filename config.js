423d4b337e36905f324421fd73496f52d450c161
felipebsales71-cmd <felipebsales71@gmail.com>
2026-08-25 10:38:42 -0400

    V5: remove legacy branding and Instagram injection

--- config.js
+++ config.js
@@ -5,15 +5,3 @@ window.SALES_TECH_CONFIG = {
   instagramUrl: "https://instagram.com/salestech.rr",
   demoVideos: []
 };
-
-(() => {
-  const stylesheet = document.createElement('link');
-  stylesheet.rel = 'stylesheet';
-  stylesheet.href = 'instagram-v2.css?v=2.0.0';
-  document.head.appendChild(stylesheet);
-
-  const brandScript = document.createElement('script');
-  brandScript.src = 'brand.js?v=20260825-4';
-  brandScript.async = false;
-  document.head.appendChild(brandScript);
-})();
